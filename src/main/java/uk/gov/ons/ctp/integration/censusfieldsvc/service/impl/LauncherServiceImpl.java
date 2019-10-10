46bd5a60af111a90da5a6d5bead168b7a0a13a49
cookel2 <eleanor.cook@ons.gov.uk>
2019-10-10 13:03:28 +0100

    remove some of the Spring Boot magic to make it more clear where the caseServiceClient comes from

--- src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/service/impl/LauncherServiceImpl.java
+++ src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/service/impl/LauncherServiceImpl.java
@@ -1,26 +1,27 @@
 package uk.gov.ons.ctp.integration.censusfieldsvc.service.impl;
 
+import com.godaddy.logging.Logger;
+import com.godaddy.logging.LoggerFactory;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.godaddy.logging.Logger;
-import com.godaddy.logging.LoggerFactory;
-
 import uk.gov.ons.ctp.integration.caseapiclient.caseservice.CaseServiceClientServiceImpl;
 import uk.gov.ons.ctp.integration.caseapiclient.caseservice.model.CaseContainerDTO;
-import uk.gov.ons.ctp.integration.censusfieldsvc.endpoint.LauncherEndpoint;
 import uk.gov.ons.ctp.integration.censusfieldsvc.service.LauncherService;
 
 @Service
-public class LauncherServiceImpl implements LauncherService { 
-	
+public class LauncherServiceImpl implements LauncherService {
+
   private static final Logger log = LoggerFactory.getLogger(LauncherServiceImpl.class);
 
   @Autowired private CaseServiceClientServiceImpl caseServiceClient;
 
   @Override
   public String getEqUrl(UUID caseId) {
+    log.with("caseId", caseId)
+        .debug(
+            "getEqUrl() calling Case Service to get the details required "
+                + "for launching the questionnaire in EQ");
     CaseContainerDTO caseDetails = caseServiceClient.getCaseById(caseId, false);
     log.info("The case details received are: " + caseDetails);
     return null;
