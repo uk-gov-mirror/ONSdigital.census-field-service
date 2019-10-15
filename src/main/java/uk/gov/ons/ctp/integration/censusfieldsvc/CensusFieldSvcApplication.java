d3519dccdb5360cb7bc9a48bc6efd29a470bf4c3
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-10-15 09:00:04 +0100

    CR-568 Code reformatting changes

--- src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/CensusFieldSvcApplication.java
+++ src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/CensusFieldSvcApplication.java
@@ -138,49 +138,47 @@ public class CensusFieldSvcApplication {
       LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
     }
   }
-  
+
   @Configuration
   public static class MyServiceProviderConfig extends ServiceProviderConfigurerAdapter {
 
-      @Override
-      public void configure(HttpSecurity http) throws Exception {
-          http.authorizeRequests()
-                  .regexMatchers("/")
-                  .permitAll()
-                  .antMatchers("/completed")
-                  .permitAll()
-                  .antMatchers("/info")
-                  .permitAll()
-                  .antMatchers("/hello3")
-                  .permitAll()
-                  .regexMatchers("/anon/hello")
-                  .permitAll();
-      }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+          .regexMatchers("/")
+          .permitAll()
+          .antMatchers("/completed")
+          .permitAll()
+          .antMatchers("/info")
+          .permitAll()
+          .antMatchers("/hello3")
+          .permitAll()
+          .regexMatchers("/anon/hello")
+          .permitAll();
+    }
 
-      @Override
-      public void configure(ServiceProviderBuilder serviceProvider) throws Exception {
-          // @formatter:off
-          serviceProvider
-              .metadataGenerator()
-              .entityId("localhost")
+    @Override
+    public void configure(ServiceProviderBuilder serviceProvider) throws Exception {
+      serviceProvider
+          .metadataGenerator()
+          .entityId("localhost")
           .and()
-              .sso()
+          .sso()
           .and()
-              .logout()
-              .defaultTargetURL("/afterlogout")
+          .logout()
+          .defaultTargetURL("/afterlogout")
           .and()
-              .metadataManager()
-              .metadataLocations("classpath:/GoogleIDPMetadata-test.field.census.gov.uk.xml")
-              .defaultIDP("https://accounts.google.com/o/saml2?idpid=C00n4re6c")
-              .refreshCheckInterval(0)
+          .metadataManager()
+          .metadataLocations("classpath:/GoogleIDPMetadata-test.field.census.gov.uk.xml")
+          .defaultIDP("https://accounts.google.com/o/saml2?idpid=C00n4re6c")
+          .refreshCheckInterval(0)
           .and()
-              .extendedMetadata()
-              .idpDiscoveryEnabled(false)  // disable IDP selection page
+          .extendedMetadata()
+          .idpDiscoveryEnabled(false) // disable IDP selection page
           .and()
-              .keyManager()
-              .privateKeyDERLocation("classpath:/localhost.key.der")
-              .publicKeyPEMLocation("classpath:/localhost.cert");
-          // @formatter:on
-      }
+          .keyManager()
+          .privateKeyDERLocation("classpath:/localhost.key.der")
+          .publicKeyPEMLocation("classpath:/localhost.cert");
+    }
   }
 }
