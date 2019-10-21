11f0f1fa0b48ff3efc324ac7702397909f444a7b
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-10-21 16:13:44 +0100

    CR-568 Added debug page to show the request headers

---
+++ src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/endpoint/DebugController.java
@@ -0,0 +1,30 @@
+package uk.gov.ons.ctp.integration.censusfieldsvc.endpoint;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DebugController {
+
+  @RequestMapping("/debug")
+  public ModelAndView home(HttpServletRequest request, @RequestParam(required = false) String caseId) {
+    HashMap<String, String> headerMap = new HashMap<>();
+    
+    Enumeration<String> names = request.getHeaderNames();
+    while (names.hasMoreElements()) {
+      String headerName = names.nextElement();
+      String headerValue = request.getHeader(headerName);
+     
+      headerMap.put(headerName, headerValue);
+    }    
+    
+    ModelAndView homeView = new ModelAndView("debug");
+    homeView.addObject("headers", headerMap);
+    return homeView;
+  }
+}
