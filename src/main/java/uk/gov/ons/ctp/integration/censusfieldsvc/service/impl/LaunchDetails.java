1e35148303469f3d6586041541be30d988855a6f
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-11-04 16:01:41 +0000

    CR-565 Reinstated code to publish survey launched events to Rabbit

---
+++ src/main/java/uk/gov/ons/ctp/integration/censusfieldsvc/service/impl/LaunchDetails.java
@@ -0,0 +1,15 @@
+package uk.gov.ons.ctp.integration.censusfieldsvc.service.impl;
+
+import java.util.UUID;
+import lombok.Data;
+
+/**
+ * Composite object which is used to supply the user of LaunchService with details
+ * about the redirection to EQ.
+ */
+@Data
+public class LaunchDetails {
+  private String eqUrl;
+  private String questionnaireId;
+  private UUID caseId;
+}
